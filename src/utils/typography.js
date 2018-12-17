10a9752b32388cb0add2d368afb77618aa192da1
Ryan Kubik <rmkubik@me.com>
2018-12-16 20:27:31 -0800

    Add TODO to make anchor color an option in sutro theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,8 @@ import sutro from 'typography-theme-sutro';
 
 const typography = new Typography(sutro);
 
+// TODO: FORK AND/OR Sutro style to make anchorColor a style option
+
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
   typography.injectStyles();
